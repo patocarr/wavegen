8eca5677a64c5e2a2c111feade1dcd1b14721422
Pat Carr <pat@tuxengineering.com>
2024-11-18 19:10:45 -0800

    Remove unused imports

--- wavegen.py
+++ wavegen.py
@@ -1,13 +1,9 @@
 #!/usr/bin/python3
 
 import tkinter as tk
-from tkinter import messagebox
-from tkinter import ttk
 
 import math
-import matplotlib.style
 from matplotlib import pyplot as plt
-import matplotlib as mpl
 import struct
 import configparser
 
